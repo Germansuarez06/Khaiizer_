4076df158768fcda23a0c70f94364355166b4f37
German Suarez <german.eduardo.suarez90@gmail.com>
2025-08-02 12:19:28 -0400

    Add files via upload

---
+++ modulo 2/HTML/examen html/JS/tarea1/ejercicio3.js
@@ -0,0 +1,12 @@
+let number1 = Number(prompt("Ingrese un Numero: "));
+let number2 = Number(prompt("Ingrese otro numero: "));
+
+let suma = number1 + number2
+let resta = number1 - number2
+let multiplicacion = number1 * number2
+let division = number1 / number2
+
+console.log(`La Suma es: ${suma}`)
+console.log(`La Resta es: ${resta}`)
+console.log(`La Multiplicacion es:${multiplicacion}`)
+console.log(`La Division es: ${division}`)
